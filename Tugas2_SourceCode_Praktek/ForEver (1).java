a6847588b2edfb85cc143bdf2d9572bfe664d978
andifadel26 <andifadel2610@gmail.com>
2026-04-25 20:11:46 +0800

    Add files via upload

---
+++ Tugas2_SourceCode_Praktek/ForEver (1).java
@@ -0,0 +1,20 @@
+/**
+ *  NIM          : 13020240002
+ *  Nama         : Andi Muhammad Fadel
+ *  Hari/Tanggal : Sabtu/25 April 2026
+ *  Waktu        : 20:10 */
+
+
+
+public class ForEver {
+
+    public static void main(String[] args) {
+
+        //program
+        System.out.println("Program akan looping, akhiri dengan ^c");
+        while (true) {
+            System.out.println("Print satu baris ....\n");
+
+        }
+    }
+}
