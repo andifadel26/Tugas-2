018fde597edbd3374618fdc05c9de9ed3c92a539
andifadel26 <andifadel2610@gmail.com>
2026-04-25 20:27:59 +0800

    Add files via upload

---
+++ Tugas2_SourceCode_Praktek/KasusBoolean (1).java
@@ -0,0 +1,23 @@
+/**
+ *  NIM          : 13020240002
+ *  Nama         : Andi Muhammad Fadel
+ *  Hari/Tanggal : Sabtu/25 April 2026
+ *  Waktu        : 21.10 */
+
+//ekspresi kondisional dengan boolean
+public class KasusBoolean {
+    public static void main(String[] args) {
+        //kamus
+        boolean bool;
+        //algoritma
+        bool = true;
+        if (bool) {
+            System.out.print("true \n");
+        }else  
+            System.out.print("false \n");
+        if (!bool) {
+            System.out.print("salah \n");
+        }else 
+            System.out.print("benar \n");
+    }   
+}
